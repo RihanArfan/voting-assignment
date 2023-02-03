200744c2337ca8bd19d8e8d9f917e47a5ab4b3c1
Rihan Arfan <rihan.arfan@fdmgroup.com>
2023-02-03 13:17:46 +0000

    Automate CreatedAt and UpdatedAt columns

--- Models/BaseModel.cs
+++ Models/BaseModel.cs
@@ -1,8 +1,14 @@
-﻿namespace Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models;
 
 public class BaseModel
 {
     public int Id { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public DateTime CreatedAt { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
     public DateTime UpdatedAt { get; set; }
 }
